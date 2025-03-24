6fccc582bf0ca35763663d082281f3c6f20450b9
Marta Prieto <G00431347@atu.ie>
2025-03-24 11:47:51 +0000

    Subtraction method and tests done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,4 +6,12 @@ public class Calculator {
         }
         return (int)result;
     }
+
+    public int sub(int firstNum, int secondNum){
+        long result = (long)firstNum - (long)secondNum;
+        if(result < Integer.MIN_VALUE){
+            throw new IllegalArgumentException("Your values are too small");
+        }
+        return (int)result;
+    }
 }
